c30e4cab1d21b65060698365790ad4cad12435db
grakify90 <ninavanes1990@hotmail.com>
2020-09-17 10:29:42 +0200

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,42 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <div id="nav">
+      <Header />
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    Todos,
-  },
-  data() {
-    return {
-      todos: [
-        { id: 1, title: "Todo One", completed: false },
-        { id: 2, title: "Todo Two", completed: true },
-        { id: 3, title: "Todo Three", completed: false },
-      ],
-    };
+    Header,
   },
 };
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: white;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input
+        type="text"
+        v-model="title"
+        name="title"
+        placeholder="Add Todo..."
+      />
+      <input type="submit" name="title" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+//library to create id's effectively
+// import { v4 as uuidv4 } from "uuid";
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        title: this.title,
+        completed: false,
+      };
+      //Send up to parent by emitting event
+      this.$emit("add-todo", newTodo);
+      //Clear form after submitting
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
